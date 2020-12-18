8550f3fec428a432f720f524e7bf4f0786ca6e56
cherifseye <casey5@ulaval.ca>
2020-12-18 00:53:41 -0500

    Hangman just a start

---
+++ test_.py
@@ -0,0 +1,14 @@
+def dis_word_guessed(secret_word, letters_guessed):
+    '''
+    secret_word: string, the word the user is guessing; assumes all letters are
+      lowercase
+    letters_guessed: list (of letters), which letters have been guessed so far;
+      assumes that all letters are lowercase
+    returns: boolean, True if all the letters of secret_word are in letters_guessed;
+      False otherwise
+    '''
+    l = []
+    for letter in secret_word:
+        if letter in letters_guessed:
+            l.append(letter)
+    return liste   
